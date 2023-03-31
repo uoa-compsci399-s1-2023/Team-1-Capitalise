3fabd080caaf095b9131ecc7e3a6df0a7533460d
lucaseng <lucasengnz@outlook.com>
2023-03-31 23:18:51 +1300

    Modified POST requests for Users and Projects.

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const {Project, validate} = require('../models/project');
-const {User} = require('../models/user');
+const { Project, validate } = require('../models/project');
+const { User } = require('../models/user');
 
 //Get all projects
 const getAllProjects = async (req, res) => {
@@ -12,62 +12,62 @@ const getAllProjects = async (req, res) => {
 const getProjectsByLikes = async (req, res) => {
     const projects = await Project.find().sort('likes');
     res.send(projects)
-  }
+}
 
 
 // get projects by badge
 const getProjectByBadge = async (req, res) => {
-    const {badge} = req.params;
+    const { badge } = req.params;
     //need to check badge
-    res.status(200).json({badge: `${badge}`});
-  }
+    res.status(200).json({ badge: `${badge}` });
+}
 
 
 //find project by Id
 const getProject = async (req, res) => {
-    const {projectId} = req.params;
-    
+    const { projectId } = req.params;
+
     //Checks if the paramter projectId is a valid Id i.e long enough
-    if(!mongoose.Types.ObjectId.isValid(projectId)){
-        return res.status(404).json({err: "No projectid found"});
+    if (!mongoose.Types.ObjectId.isValid(projectId)) {
+        return res.status(404).json({ err: "No projectid found" });
     }
-    
+
     const project = await Project.findById(projectId);
-  
+
     //If no project exist
-    if(!project){
-        return res.status(404).json({err: "No project found"});
+    if (!project) {
+        return res.status(404).json({ err: "No project found" });
     }
     //If a project exist
     res.status(200).json(project);
+}
+
+
+
+const updateProjectById = async (req, res) => {
+    const { projectId } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(projectId)) {
+        return res.status(404).json({ err: "No project found" });
     }
 
-
-
-const updateProjectById =  async(req, res) => {
-    const {projectId} = req.params;
-    if(!mongoose.Types.ObjectId.isValid(projectId)){
-        return res.status(404).json({err: "No project found"});
-    }
-  
     //Changes what ever is different
     //Doesnt add any additional tags
-    const project = await Project.findOneAndUpdate({_id : projectId}, {...req.body});
-  
-    if (!project){
-        return res.status(404).json({err: "No Project found"});
+    const project = await Project.findOneAndUpdate({ _id: projectId }, { ...req.body });
+
+    if (!project) {
+        return res.status(404).json({ err: "No Project found" });
     }
     res.status(200).json(project);
-  }
+}
 
 
 const addNewProject = async (req, res) => {
-    const { error } = validate(req.body); 
+    const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
     const user = await User.findById(req.body.userId);
     if (!user) return res.status(400).send('Invalid user.');
-  
+
     let project = new Project({
         name: req.body.name,
         semester: req.body.semester,
@@ -75,57 +75,77 @@ const addNewProject = async (req, res) => {
         members: {
             _id: user._id,
             name: user.name
-          },
+        },
         content: req.body.content,
         likes: 0,
         badges: req.body.badges,
         tags: req.body.tags
     });
 
+
+    //Add project to user's project attribute. 
+    user.project = {
+        _id: project._id,
+        projectName: project.name
+    };
+
     project = await project.save();
-    
+    updateUser = await user.save();
+
     res.send(project);
-  }
+}
 
 
 const addUserToProject = async (req, res) => {
-  
-    const user = await User.findById(req.params.userid);
+
+    const myProj = await Project.findById(req.params.id);
+
+
+    //Adds the project to the User.
+    const user = await User.findByIdAndUpdate(req.params.userid, {
+        project: {
+            _id: myProj._id,
+            projectName: myProj.name
+        }
+    });
+
     if (!user) return res.status(400).send('Invalid user.');
 
+    //Appends i.e. pushes the user onto the members.
     const project = await Project.findByIdAndUpdate(req.params.id,
-        { 
-            //I dont know what this does, but yes
-            $push: {members: user}
+        {
+            //Appends
+            $push: { members: user }
         }, { new: true });
-    
-      if (!project) return res.status(404).send('The project with the given ID was not found.');
-      
-      res.send(project);
-  }
+
+    if (!project) return res.status(404).send('The project with the given ID was not found.');
+
+
+    res.send(project);
+}
 
 
 const deleteProject = async (req, res) => {
-    const {projectId} = req.params
+    const { projectId } = req.params
 
-    const project = Project.findById({_id : projectId})
+    const project = Project.findById({ _id: projectId })
 
     //differenet Id type from db id 
-    if(!mongoose.Types.ObjectId.isValid(projectId)){
-        return res.status(404).json({err: "Wrong type of id "});
-    } 
-    //If no project is found
-    if (!project){
-        return res.status(404).json({err: "No Project found"});
+    if (!mongoose.Types.ObjectId.isValid(projectId)) {
+        return res.status(404).json({ err: "Wrong type of id " });
     }
-    
+    //If no project is found
+    if (!project) {
+        return res.status(404).json({ err: "No Project found" });
+    }
+
     /*****Find the user where projectId : projectId*****/
     const users = await User.find()
 
     /*****Then delete the project it self*****/
 
 
-    res.status(200).json({user: users, del : 'del'});
+    res.status(200).json({ user: users, del: 'del' });
 }
 
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,98 +1,103 @@
-const {User, validate} = require('../models/user');
-const {Project} = require('../models/project');
+const { User, validate } = require('../models/user');
+const { Project } = require('../models/project');
 const bcrypt = require('bcrypt');
 
 
 //Gets all users and sorts by name
 const getAllUsers = async (req, res) => {
-    const users = await User.find().sort('name');
-    res.send(users);
+  const users = await User.find().sort('name');
+  res.send(users);
 }
 
 //Get user by username
 const getUserByName = async (req, res) => {
-    const {user} = req.params
-    
-    const username = await User.findOne({username: user});
-    if(!username){
-      return res.status(404).json({fail: `no user with ${user} found`})
-    }
-    
-    res.send(username);
+  const { user } = req.params
+
+  const username = await User.findOne({ username: user });
+  if (!username) {
+    return res.status(404).json({ fail: `no user with ${user} found` })
   }
+
+  res.send(username);
+}
 
 //Get user by Id
 const getUserById = async (req, res) => {
-    const {id} = req.params
-    
-    const username = await User.findOne({_id: id});
-    if(!username){
-      return res.status(404).json({fail: `no user with ${id} found`})
-    }
-    
-    res.send(username);
+  const { id } = req.params
+
+  const username = await User.findOne({ _id: id });
+  if (!username) {
+    return res.status(404).json({ fail: `no user with ${id} found` })
   }
+
+  res.send(username);
+}
 
 
 //Adds new user
 const postUser = async (req, res) => {
-    const { error } = validate(req.body); 
-    if (error) return res.status(400).send(error.details[0].message);
-    let user = "";
-    let password = await bcrypt.hash(req.body.password, 10);
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  let user = "";
+  let password = await bcrypt.hash(req.body.password, 10);
 
-    if (req.body.projectId) {
-      const project = await Project.findById(req.body.projectId);
-      if (!project) return res.status(400).send('Invalid project.');
-      
-      user = new User({ 
-        name: req.body.name, 
-        email: req.body.email,
-        username: req.body.username,
-        password: password,
-        github: req.body.github,
-        linkedin: req.body.linkedin,
-        project: {
-          _id: project._id,
-          projectName: project.name
-        },
-        bio: req.body.bio,
-        likedProjects: [],
-        myComments: [],
-        userType: req.body.userType
-      });
-    } else {
-      user = new User({ 
-        name: req.body.name, 
-        email: req.body.email,
-        username: req.body.username,
-        password: password,
-        github: req.body.github,
-        linkedin: req.body.linkedin,
-        bio: req.body.bio,
-        likedProjects: [],
-        myComments: [],
-        userType: req.body.userType
-      });
+  if (req.body.projectId) {
+    const project = await Project.findById(req.body.projectId);
+    if (!project) return res.status(400).send('Invalid project.');
 
-    }
+    user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      username: req.body.username,
+      password: password,
+      github: req.body.github,
+      linkedin: req.body.linkedin,
+      project: {
+        _id: project._id,
+        projectName: project.name
+      },
+      bio: req.body.bio,
+      likedProjects: [],
+      myComments: [],
+      userType: req.body.userType
+    });
 
-  
+    //Append user to project's members 
+    const appendToProject = await Project.findByIdAndUpdate(project._id,
+      {
+        $push: { members: user }
+      }, { new: true });
 
-    user = await user.save();
-    
-    res.send(user);
+  } else {
+    user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      username: req.body.username,
+      password: password,
+      github: req.body.github,
+      linkedin: req.body.linkedin,
+      bio: req.body.bio,
+      likedProjects: [],
+      myComments: [],
+      userType: req.body.userType
+    });
+
   }
+
+  user = await user.save();
+
+  res.send(user);
+}
 
 //updates the user details besides the user type
 const updateUserDetails = async (req, res) => {
-    const {username} = req.params;
-  }
+  const { username } = req.params;
+}
 
 module.exports = {
-    getAllUsers,
-    getUserByName,
-    getUserById,
-    postUser,
-    updateUserDetails,
+  getAllUsers,
+  getUserByName,
+  getUserById,
+  postUser,
+  updateUserDetails,
 }
