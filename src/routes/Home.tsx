061cb5fc61ad8701dfc2690fafd1164c2c518074
AlbertLinNZ <ablin158@gmail.com>
2023-04-02 20:25:50 +1200

    initial setup

---
+++ src/routes/Home.tsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+import logo from "../assets/Logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <img className="logo" src={logo} alt="logo"></img>
+    </div>
+  );
+}
+
+export default App;
