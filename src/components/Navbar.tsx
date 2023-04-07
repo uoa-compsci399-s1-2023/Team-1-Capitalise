aaf8589682e6a0a155cfc387d45ab479c01660d1
NoGraphite <dqua365@aucklanduni.ac.nz>
2023-04-08 00:38:01 +1200

    Added in SEARCH/SIGN UP/LOGIN responsive functionality
    Added Comments

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,28 @@
-import * as React from 'react';
-import { AppBar, Box, Button, Menu, Toolbar, styled } from "@mui/material";
-import {Routes, Route, useNavigate, Navigate} from 'react-router-dom';
+import * as React from "react";
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  Menu,
+  Toolbar,
+  Tooltip,
+  styled,
+} from "@mui/material";
+import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
 import Logo from "../assets/Logo.svg";
 import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
 import zIndex from "@mui/material/styles/zIndex";
-import Container from '@mui/material/Container';
-import IconButton from '@mui/material/IconButton';
-import MenuItem from '@mui/material/MenuItem';
-import MenuIcon from '@mui/icons-material/Menu';
-import Typography from '@mui/material/Typography';
+import Container from "@mui/material/Container";
+import IconButton from "@mui/material/IconButton";
+import MenuItem from "@mui/material/MenuItem";
+import MenuIcon from "@mui/icons-material/Menu";
+import Typography from "@mui/material/Typography";
+import { alignProperty } from "@mui/material/styles/cssUtils";
 
-const pages = ['About', 'Projects'];
-
+const pages = ["About", "Projects"];
+const settings = ["Login", "Register"];
 
 const StyledToolBar = styled(Toolbar)({
   height: "75px",
@@ -22,47 +32,69 @@ const StyledToolBar = styled(Toolbar)({
   alignItems: "center",
   padding: "12px",
 });
-const StyledLink = styled(Link)({
-  display: 'block',
-  padding: '25px',
-  color: '#3c3c3c',
-  textDecoration: 'none',
-  fontSize: 15,
-  '&:hover': {
-    backgroundColor: '#c0e4ff',
-  },
-});
 
+const NavButtons = styled(Button)({
+  my: 2,
+  color: "black",
+  display: "block",
+  fontSize: 25,
+  fontFamily: "Roboto",
+  fontWeight: 400,
+});
+{/*Navigation Bar*/}
 function ResponsiveAppBar() {
-    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
-  
-    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-      setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-      setAnchorElUser(event.currentTarget);
-    };
-  
-    const handleCloseNavMenu = () => {
-      setAnchorElNav(null);
-    };
-  
-    const handleCloseUserMenu = () => {
-      setAnchorElUser(null);
-    };
-    const navigate = useNavigate();
-    const goToPage = (pageName: any) => {
-      navigate('/' + pageName);
-    };
-  
+  {/*Functionality for opening/closing sidebar*/}
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+  {/*Navigation Functionality + Routing*/}
+  const navigate = useNavigate();
+  const goToPage = (pageName: any) => {
+    navigate("/" + pageName);
+  };
+  {/*App Bar*/}
   return (
     <AppBar position="static" sx={{ bgcolor: "white", zIndex: "100" }}>
-      <Container maxWidth= {false} disableGutters>
+      <Container maxWidth={false} disableGutters>
         <StyledToolBar>
-          <Box height="100%" display="flex" gap="25px" >
-            
-            <Box gap = "25px" sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          {/*Desktop Logo*/}
+          <Box height="100%" display="flex" gap="25px">
+            <Link to="/">
+              <Box
+                component="img"
+                src={Logo}
+                alt="logo"
+                padding="5px 100px"
+                sx={{
+                  flexGrow: 1,
+                  height: "100%",
+                  display: { xs: "none", md: "flex" },
+                }}
+              ></Box>
+            </Link>
+            {/*This is the side bar for mobile*/}
+            <Box
+              gap="25px"
+              sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+            >
               <IconButton
                 size="large"
                 aria-label="account of current user"
@@ -77,68 +109,120 @@ function ResponsiveAppBar() {
                 id="menu-appbar"
                 anchorEl={anchorElNav}
                 anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'left',
+                  vertical: "bottom",
+                  horizontal: "left",
                 }}
                 keepMounted
                 transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'left',
+                  vertical: "top",
+                  horizontal: "left",
                 }}
                 open={Boolean(anchorElNav)}
                 onClose={handleCloseNavMenu}
                 sx={{
-                  display: { xs: 'block', md: 'none' },
+                  display: { xs: "block", md: "none" },
                 }}
               >
                 {pages.map((page) => (
-                  <MenuItem key={page} onClick={() => {
-                    handleCloseNavMenu();
-                    goToPage(page);
-                    }}>
+                  <MenuItem
+                    key={page}
+                    onClick={() => {
+                      handleCloseNavMenu();
+                      goToPage(page);
+                    }}
+                  >
                     <Typography textAlign="center">{page}</Typography>
                   </MenuItem>
                 ))}
               </Menu>
             </Box>
+            {/*Mobile Logo*/}
             <Link to="/">
-              <img
+              <Box
+                component="img"
                 src={Logo}
                 alt="logo"
-                height="100%"
-                style={{ verticalAlign: "middle" }}
-              ></img>
+                sx={{
+                  flexGrow: 1,
+
+                  padding:"5px 100px",
+                  height: "100%",
+                  display: { xs: "flex", md: "none" },
+                 
+                }}
+              ></Box>
             </Link>
+
             {/* The nav barpage links*/}
-            
-            <Box sx={{ justifyContent: "center", alignItems: "center",flexGrow: 1, display: { xs: 'none', md: 'flex' }}}>
-            {pages.map((page) => (
-              <Button 
-                key={page}
-                onClick={() => {
-                  handleCloseNavMenu();
-                  goToPage(page);
+            <Box
+              gap="50px"
+              sx={{
+                justifyContent: "center",
+                alignItems: "center",
+                flexGrow: 1,
+                display: { xs: "none", md: "flex" },
+              }}
+            >
+              {pages.map((page) => (
+                <NavButtons
+                  key={page}
+                  onClick={() => {
+                    handleCloseNavMenu();
+                    goToPage(page);
                   }}
-                sx={{ my: 2, color: 'black', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
+                >
+                  {page}
+                </NavButtons>
+              ))}
             </Box>
-            
           </Box>
-          <Box gap="20px" sx={{ justifyContent: "right", flexGrow: 1, display: { xs: 'none', md: 'flex' }}}>
-              <SearchBar />
-              <Button variant="outlined">Log in</Button>
-              <Button variant="contained">Sign up</Button>
-            </Box>
-          {/* This is for the left hand side of the Nav Bar*/}
-          
-          
+          {/* This is for the right hand side of the Nav Bar*/}
+          <Box
+            gap="20px"
+            sx={{
+              justifyContent: "right",
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+            }}
+          >
+            <SearchBar />
+            <Button variant="outlined">Log in</Button>
+            <Button variant="contained">Sign up</Button>
+          </Box>
+          {/*RHS for Mobile/Smaller Screens*/}
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none"}, justifyContent: 'flex-end'}}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Not Logged In" src="" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
         </StyledToolBar>
       </Container>
     </AppBar>
   );
-};  
+}
 
 export default ResponsiveAppBar;
