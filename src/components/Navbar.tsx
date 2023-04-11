535c84287f1eb4259c28a33399a28b14d2133957
Daniel Quach <dqua365@aucklanduni.ac.nz>
2023-04-11 23:41:57 +1200

    Navbuttons : Font Size  25->19 textTransform: Capitalize
    SignIn/SignUp buttons now have their own AuthButton styled()
    
    Removed unnecessary code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,3 @@
-// Yathi - Changed position to "sticky" and changed height to 8vh to calculate sidebar height
-
 import * as React from "react";
 import {
   AppBar,
@@ -22,24 +20,29 @@ import IconButton from "@mui/material/IconButton";
 import MenuItem from "@mui/material/MenuItem";
 import MenuIcon from "@mui/icons-material/Menu";
 import Typography from "@mui/material/Typography";
-import { alignProperty } from "@mui/material/styles/cssUtils";
 import { AppRegistration, Login } from "@mui/icons-material";
 
 const pages = ["About", "Projects"];
-const settings = ["Login", "Register"];
 
 const StyledToolBar = styled(Toolbar)({
   height: "8vh",
   padding: "2px 10%",
   color: "black",
+
 });
 
-const NavButtons = styled(Button)({
+const NavButton = styled(Button)({
   color: "black",
   display: "block",
-  fontSize: 25,
+  fontSize: 19,
   fontFamily: "Roboto",
   fontWeight: 400,
+  textTransform: "capitalize"
+});
+const AuthButton = styled(Button)({
+  whiteSpace: "nowrap",
+  overflow: "hidden",
+  padding: "0 25px"
 });
 {
   /*Navigation Bar*/
@@ -61,17 +64,13 @@ function ResponsiveAppBar() {
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget);
   };
-
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
-
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
-
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-
   const open = Boolean(anchorEl);
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -95,134 +94,143 @@ function ResponsiveAppBar() {
       <Container maxWidth="xl" disableGutters>
         <StyledToolBar disableGutters>
           {/*Desktop Logo*/}
-   
-            <Link to="/">
-              <Box
-                padding = "0 30px"
-                component="img"
-                src={Logo}
-                alt="logo"
-                sx={{
-                  width: "200px",
-                  height: "auto",
-                  flexGrow: 1,
-                  display: { xs: "none", md: "flex" },
-                }}
-              ></Box>
-            </Link>
-
-            {/* The nav barpage links*/}
+          <Link to="/">
             <Box
-              gap="15px"
+              padding="0 30px"
+              component="img"
+              src={Logo}
+              alt="logo"
               sx={{
+                width: "200px",
+                height: "auto",
                 flexGrow: 1,
                 display: { xs: "none", md: "flex" },
               }}
+            ></Box>
+          </Link>
+
+          {/* The nav barpage links*/}
+          <Box
+            gap="15px"
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+            }}
+          >
+            {pages.map((page) => (
+              <NavButton
+                key={page}
+                onClick={() => {
+                  handleCloseNavMenu();
+                  goToPage(page);
+                }}
+              >
+                {page}
+              </NavButton>
+            ))}
+          </Box>
+
+          {/* This is for the right hand side of the Nav Bar*/}
+          <Box
+            gap="20px"
+            sx={{
+              display: { xs: "none", md: "flex" },
+            }}
+          >
+            <SearchBar />
+            <AuthButton
+              onClick={() => {
+                goToPage("login");
+              }}
+              variant="outlined"
+            >
+              Log In
+            </AuthButton>
+            <AuthButton
+              onClick={() => {
+                goToPage("register");
+              }}
+              variant="contained"
+            >
+              Sign Up
+            </AuthButton>
+          </Box>
+
+          {/*This is the side bar for mobile*/}
+          <Box
+            gap="25px"
+            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+          >
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
             >
               {pages.map((page) => (
-                <NavButtons
+                <MenuItem
                   key={page}
                   onClick={() => {
                     handleCloseNavMenu();
                     goToPage(page);
                   }}
                 >
-                  {page}
-                </NavButtons>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
               ))}
-            </Box>
-   
-          
-          {/* This is for the right hand side of the Nav Bar*/}
-          <Box
-            gap="20px"
-            sx={{
-            
-              display: { xs: "none", md: "flex" },
-            }}
-          >
-            <SearchBar />
-            <Button sx={{whiteSpace: "nowrap", overflow: "hidden"}} onClick={()=> { goToPage("login")}}variant="outlined">Log In</Button>
-            <Button  sx={{whiteSpace: "nowrap", overflow: "hidden"}} onClick={()=> { goToPage("register")}}variant="contained">Sign Up</Button>
+              <MenuItem
+                onClick={() => {
+                  handleCloseNavMenu();
+                  goToPage("/search");
+                }}
+              >
+                <Typography textAlign="center">Search</Typography>
+              </MenuItem>
+            </Menu>
           </Box>
-
-          
-          {/*This is the side bar for mobile*/}
-          <Box
-              gap="25px"
-              sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
-            >
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: "block", md: "none" },
-                }}
-              >
-                {pages.map((page) => (
-                  <MenuItem
-                    key={page}
-                    onClick={() => {
-                      handleCloseNavMenu();
-                      goToPage(page);
-                    }}
-                  >
-                    <Typography textAlign="center">{page}</Typography>
-                  </MenuItem>
-                  
-                ))}
-                  <MenuItem 
-                      onClick={() => {
-                      handleCloseNavMenu();
-                      goToPage("/search")}}> 
-                    <Typography textAlign="center">Search</Typography>
-                  </MenuItem>
-              </Menu>
-            </Box>
           {/*Mobile Logo*/}
 
           <Link to="/">
-              <Box
-                component="img"
-                src={Logo}
-                alt="logo"
-                sx={{
-          
-                  width: "200px",
-                  height: "auto",
-                  flexGrow: 1,
-                  display: { xs: "flex", md: "none" },
-                 
-                }}
-              ></Box>
-            </Link>
+            <Box
+              component="img"
+              src={Logo}
+              alt="logo"
+              sx={{
+                width: "200px",
+                height: "auto",
+                flexGrow: 1,
+                display: { xs: "flex", md: "none" },
+              }}
+            ></Box>
+          </Link>
           {/*RHS for Mobile/Smaller Screens*/}
           <Box
             sx={{
               flexGrow: 1,
               display: { xs: "flex", md: "none" },
-              justifyContent: "right"
+              justifyContent: "right",
             }}
           >
             <Tooltip title="Open settings">
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-<<<<<<< HEAD
-import Home from "./routes/Home";
-import About from "./routes/About";
-import "@fontsource/inter";
-import Projects from "./routes/Projects";
-=======
 import "./index.css";
 import "@fontsource/inter"; // Don't use this. Will remove later.
 import { ThemeProvider } from "./mui";
 import { Home, Projects, About } from "./routes"
 import customTheme1 from "./themes/custom1"
 
->>>>>>> aaf597bfdc04a9becd1bd2c1970f66f0d41ff1d6
 const router = createBrowserRouter([
   {
     path: "/",
