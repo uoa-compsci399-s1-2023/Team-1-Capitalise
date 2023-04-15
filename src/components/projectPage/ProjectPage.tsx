939f5ebd96c6607c394e8d6a9f2e45cf33d9250c
rwu050 <rwu050@aucklanduni.ac.nz>
2023-04-15 23:02:18 +1200

    very basic comment display working with mock project

--- src/components/projectPage/ProjectPage.tsx
+++ src/components/projectPage/ProjectPage.tsx
@@ -1,72 +1,86 @@
-import React, { useState } from 'react'
-import ProjectDetails from './ProjectDetails';
-import ContentBlock from './ContentBlock';
-import ProjectHeader from './ProjectHeader';
-import TabButton from './TabButton';
-import { mockProject } from '../../model/MockProject';
+import React, { useState } from "react";
+import ProjectDetails from "./ProjectDetails";
+import ContentBlock from "./ContentBlock";
+import ProjectHeader from "./ProjectHeader";
+import TabButton from "./TabButton";
+import { mockProject } from "../../model/MockProject";
 
-import { Stack, Box, useTheme, Container, Typography, Button, Divider } from '@mui/material';
+import Comment from "../../components/Comment";
 
+import {
+  Stack,
+  Box,
+  useTheme,
+  Container,
+  Typography,
+  Button,
+  Divider,
+} from "@mui/material";
 
 type TabContent = {
-  type: "gallery" | "poster" | "text" | "sectionHeading" | "video" | "codeBlock" | "quote"
-  value: string
-}
+  type:
+    | "gallery"
+    | "poster"
+    | "text"
+    | "sectionHeading"
+    | "video"
+    | "codeBlock"
+    | "quote";
+  value: string;
+};
 
 export default function ProjectPage() {
-  const theme = useTheme()
+  const theme = useTheme();
 
   const [selectedTab, setSelectedTab] = useState(0);
 
   let imgs: string[] = [];
   if (mockProject.content.length > 0) {
-    imgs = mockProject.content[0].tabContent[1].value
+    imgs = mockProject.content[0].tabContent[1].value;
   }
-
 
   return (
     <>
       <img
         src={mockProject.banner}
-        alt='Project cover photo'
-        width={'100%'}
+        alt="Project cover photo"
+        width={"100%"}
         height={150}
         style={{
-          objectFit: 'cover'
+          objectFit: "cover",
         }}
       />
 
       <Stack
         // bgcolor={theme.customColors.bgGrey}
-        bgcolor={'white'}
-        minHeight={'92vh'}
+        bgcolor={"white"}
+        minHeight={"92vh"}
         px={6}
-        maxWidth={'1600px'}
-        mx={'auto'}
+        maxWidth={"1600px"}
+        mx={"auto"}
       >
-
         <ProjectHeader
           name={mockProject.name}
           blurb={mockProject.blurb}
           likes={mockProject.likes}
         />
 
-        <Stack flexDirection={'row'} mt={2}>
-          <Stack flex={1} alignItems={'center'} mr={4} gap={2} mb={10} >
+        <Stack flexDirection={"row"} mt={2}>
+          <Stack flex={1} alignItems={"center"} mr={4} gap={2} mb={10}>
             {/* Content goes here */}
 
-            <Stack 
-              flexDirection={'row'} 
-              justifyContent={'center'} 
-              mb={6} 
-              pb={3} 
-              borderBottom={`2px solid ${theme.customColors.DividerGrey}`} 
-              width={'90%'}
+            <Stack
+              flexDirection={"row"}
+              justifyContent={"center"}
+              mb={6}
+              pb={3}
+              borderBottom={`2px solid ${theme.customColors.DividerGrey}`}
+              width={"90%"}
             >
               {mockProject.content.map((tab, index) => (
                 <TabButton
                   key={index}
-                  isSelected={selectedTab===index}
+                  isSelected={selectedTab === index}
                   value={tab.tabName}
                   setSelected={setSelectedTab}
                   index={index}
@@ -78,11 +92,18 @@ export default function ProjectPage() {
             {mockProject.content[selectedTab].tabContent.map((cb, index) => (
               <ContentBlock key={index} {...cb} />
             ))}
-
           </Stack>
           <ProjectDetails />
         </Stack>
+        <Stack mt={2} paddingLeft={4} spacing={1}>
+          <Typography variant="h1" color="initial" fontWeight={600}>
+            Comments
+          </Typography>
+          {mockProject.comments.map((comment, index) => (
+            <Comment key={index} text={comment.commentBody} />
+          ))}
+        </Stack>
       </Stack>
     </>
-  )
+  );
 }
--- src/model/MockProject.tsx
+++ src/model/MockProject.tsx
@@ -1,64 +1,74 @@
-import { TProject } from '../api/getProjects'
+import { TProject } from "../api/getProjects";
 
 export type TMockProject = {
-    _id: string;
-    timestamp?: number; // Added when backend recieves project.
-    name: string;
-    blurb?: string;
-    semester: {
-      value: string;
-    };
-    category: {
-      value: string;
-    };
-    links?: {
-      type: 'github' | 'codesandbox' | 'deployedSite' | 'codepen' | 'notion' | 'kaggle'
-      value: string
-    }[];
-    teamname: string;
-    likes: number;
-    views: number;
-    banner?: string;
-    // thumbnail: string;
-
-    members: {
-      _id: string;
-      name: string;
-    }[]
-
-    content:
-    {
-      tabName: string
-      tabContent:
-      {
-        type: "gallery" | "poster" | "text" | "video" | "codeBlock" | "quote" | "image"
-        subHeading?: string
-        value: string[]  // Perhaps look into whether it can be a string[] + string?
-      }[],
-    }[]
-
-
-    badges: {
-      id: string
-      value: 'People\'s Choice' | 'Excellence' | 'Community Impact'
-      runnerUp: boolean
-      image: string;
-    }[];
-    tags: {
-      id: string;
-      name: string;
-      mentions: number;
-      projects: TProject[];
-    }[];
-    comments : {
-      id: string
-      projectId: number
-      userId: number
-      commentBody: string
-      parentComment?: number
-      timestamp: number
-    }[]
+  _id: string;
+  timestamp?: number; // Added when backend recieves project.
+  name: string;
+  blurb?: string;
+  semester: {
+    value: string;
   };
+  category: {
+    value: string;
+  };
+  links?: {
+    type:
+      | "github"
+      | "codesandbox"
+      | "deployedSite"
+      | "codepen"
+      | "notion"
+      | "kaggle";
+    value: string;
+  }[];
+  teamname: string;
+  likes: number;
+  views: number;
+  banner?: string;
+  // thumbnail: string;
+
+  members: {
+    _id: string;
+    name: string;
+  }[];
+
+  content: {
+    tabName: string;
+    tabContent: {
+      type:
+        | "gallery"
+        | "poster"
+        | "text"
+        | "video"
+        | "codeBlock"
+        | "quote"
+        | "image";
+      subHeading?: string;
+      value: string[]; // Perhaps look into whether it can be a string[] + string?
+    }[];
+  }[];
+
+  badges: {
+    id: string;
+    value: "People's Choice" | "Excellence" | "Community Impact";
+    runnerUp: boolean;
+    image: string;
+  }[];
+  tags: {
+    id: string;
+    name: string;
+    mentions: number;
+    projects: TProject[];
+  }[];
+  comments: {
+    id: string;
+    projectId: string; // changed this from number to string
+    userId: string; // changed this from number to string
+    commentBody: string;
+    parentComment?: string; // changed this from number to stirng
+    timestamp: string; // changed this from number to string
+  }[];
+};
 
 export const mockProject: TMockProject = {
   _id: "0",
@@ -66,9 +76,12 @@ export const mockProject: TMockProject = {
   blurb: "Building a neural network from scratch.",
   semester: { value: "S1 2023" },
   category: { value: "Web Development" },
-  links: [{
-    type: 'github',
-    value: "https://github.com/uoa-compsci399-s1-2023/project-team-1"}],
+  links: [
+    {
+      type: "github",
+      value: "https://github.com/uoa-compsci399-s1-2023/project-team-1",
+    },
+  ],
   teamname: "Zuckerberg Enterprises",
   members: [],
   likes: 17,
@@ -78,21 +91,24 @@ export const mockProject: TMockProject = {
     {
       tabName: "Overview",
       tabContent: [
-        { 
-          type: "gallery", value: ["src/components/projectPage/galleryImgs/img1.png",] 
+        {
+          type: "gallery",
+          value: ["src/components/projectPage/galleryImgs/img1.png"],
         },
         {
           type: "text",
           subHeading: "Description",
-          value: [`Deep Learning has become a popular topic in recent times. It involves emulating the neural structure of the human brain through a network of nodes called a Neural Network. While our brain's neurons have physical components like nucleus, dendrites, and synapses, Neural Network neurons are interconnected and have weights and biases assigned to them. A neural network typically consists of an input layer, an output layer, and one or more hidden layers. In conventional neural networks, all nodes in these layers are interconnected to form a dense network. However, there are cases where certain nodes in the network are not connected to others, which are referred to as Sparse Neural Networks. InceptionNet models for image classification use Sparse Neural Networks. The following figure illustrates the structure of a neural network.`,]
+          value: [
+            `Deep Learning has become a popular topic in recent times. It involves emulating the neural structure of the human brain through a network of nodes called a Neural Network. While our brain's neurons have physical components like nucleus, dendrites, and synapses, Neural Network neurons are interconnected and have weights and biases assigned to them. A neural network typically consists of an input layer, an output layer, and one or more hidden layers. In conventional neural networks, all nodes in these layers are interconnected to form a dense network. However, there are cases where certain nodes in the network are not connected to others, which are referred to as Sparse Neural Networks. InceptionNet models for image classification use Sparse Neural Networks. The following figure illustrates the structure of a neural network.`,
+          ],
         },
       ],
-    }, 
+    },
     {
       tabName: "Breakdown",
       tabContent: [
-        // { 
-        //   type: "gallery", value: ["src/components/projectPage/galleryImgs/img1.png",] 
+        // {
+        //   type: "gallery", value: ["src/components/projectPage/galleryImgs/img1.png",]
         // },
         // {
         //   type: "quote",
@@ -101,17 +117,43 @@ export const mockProject: TMockProject = {
         {
           type: "text",
           subHeading: "Description",
-          value: [`Deep Learning has become a popular topic in recent times. It involves emulating the neural structure of the human brain through a network of nodes called a Neural Network. While our brain's neurons have physical components like nucleus, dendrites, and synapses, Neural Network neurons are interconnected and have weights and biases assigned to them. A neural network typically consists of an input layer, an output layer, and one or more hidden layers. In conventional neural networks, all nodes in these layers are interconnected to form a dense network. However, there are cases where certain nodes in the network are not connected to others, which are referred to as Sparse Neural Networks. InceptionNet models for image classification use Sparse Neural Networks. The following figure illustrates the structure of a neural network.`,]
+          value: [
+            `Deep Learning has become a popular topic in recent times. It involves emulating the neural structure of the human brain through a network of nodes called a Neural Network. While our brain's neurons have physical components like nucleus, dendrites, and synapses, Neural Network neurons are interconnected and have weights and biases assigned to them. A neural network typically consists of an input layer, an output layer, and one or more hidden layers. In conventional neural networks, all nodes in these layers are interconnected to form a dense network. However, there are cases where certain nodes in the network are not connected to others, which are referred to as Sparse Neural Networks. InceptionNet models for image classification use Sparse Neural Networks. The following figure illustrates the structure of a neural network.`,
+          ],
         },
       ],
-    }
+    },
   ],
-  badges: [{
-    id: '1',
-    value: 'Excellence',
-    runnerUp: false,
-    image: ''
-  }],
-  comments: [],
-  tags: []
-}
+  badges: [
+    {
+      id: "1",
+      value: "Excellence",
+      runnerUp: false,
+      image: "",
+    },
+  ],
+  comments: [
+    {
+      id: "0",
+      projectId: "0",
+      userId: "0",
+      commentBody: "This is a test comment",
+      timestamp: "15/04/2023",
+    },
+    {
+      id: "1",
+      projectId: "0",
+      userId: "1",
+      commentBody: "Daniel Ricciardo is a g",
+      timestamp: "15/04/2023",
+    },
+    {
+      id: "2",
+      projectId: "0",
+      userId: "1",
+      commentBody: "JK Charles Leclerc WDC 2023",
+      timestamp: "15/04/2023",
+    },
+  ],
+  tags: [],
+};
