8090add809ee2d778479c92c0bce8c39c754fbef
lucaseng <lucasengnz@outlook.com>
2023-04-17 18:41:17 +1200

    Updated 403 message

--- middleware/graduate.js
+++ middleware/graduate.js
@@ -1,9 +1,9 @@
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv').config();
 
-//Authorization for admin users. Simply checks the userType stored in the payload of the JSON WEB TOKEN. 
+//Authorization for graduate users. Simply checks the userType stored in the payload of the JSON WEB TOKEN. 
 function graduate(req, res, next) {
-    if (req.user.userType === "visitor") return res.status(403).send('Access Denied.');
+    if (req.user.userType === "visitor") return res.status(403).send('Access Denied. You do not have the relevant permissions to access this resource!');
     next();
 }
 
