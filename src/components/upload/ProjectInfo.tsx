819b0463488d3db58d5c13334b3761ab8340a12e
Daniel Quach <dqua365@aucklanduni.ac.nz>
2023-05-03 17:28:03 +1200

    Implemented Link dropdown+ textfields

--- src/components/upload/ProjectInfo.tsx
+++ src/components/upload/ProjectInfo.tsx
@@ -4,40 +4,46 @@ import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import { Autocomplete, Button, Chip } from '@mui/material';
+import { Autocomplete, Box, Button, Chip, FormControl, InputLabel, MenuItem, Select, styled } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import { useState } from 'react';
+import ProjectLinksForm from '../upload/ProjectLinks';
+import { Form } from 'react-router-dom';
 const projectTags = [
   { tag: 'Mobile'},
   
 ];
+
+
 export default function ProjectInfoForm() {
+  
+
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
         1. Project Details
       </Typography>
-      <Grid container spacing={2}>
       
+      <Grid container spacing={2}>
+      <Box component="form">
         <Grid item xs={12}>
-        <Typography variant="subtitle1">
-        Project Name
-        </Typography>
+      
           <TextField
             required
             id="projectName"
             name="projectName"
-           
+            label="Project Name"
             fullWidth
-            variant="standard"
+            variant="outlined"
           />
         </Grid>
         <Grid item xs={12} >
-        <Typography variant="subtitle1">
-        Project Tags
-        </Typography>
+       
         <Autocomplete
         multiple
+        
         id="tags-filled"
+
         options={projectTags.map((option) => option.tag)}
         freeSolo
         renderTags={(value: readonly string[], getTagProps) =>
@@ -46,10 +52,10 @@ export default function ProjectInfoForm() {
           ))
         }
         renderInput={(params) => (
-          <TextField
+          <TextField 
             {...params}
-            variant="standard"
-            
+            variant="outlined"
+            label="Project Tags"
     
           />
         )}
@@ -62,36 +68,14 @@ export default function ProjectInfoForm() {
             name="projectDesc"
             label="Project Description"
             fullWidth
-            variant="filled"
+            variant="outlined"
             
             multiline={true}
             rows={10}
           />
         </Grid>
-        <Grid item xs={6}>
-          <Typography variant="subtitle2">
-          GitHub Repository
-          </Typography>
-          <TextField
-            id="github"
-            name="github"
-            fullWidth
-            
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Typography variant="subtitle2">
-          CodePen
-          </Typography>
-          <TextField
-            id="github"
-            name="github"
-            fullWidth
-            
-            variant="standard"
-          />
-        </Grid>
+        
+       
         <Grid item xs={12} sm={6}>
         <Typography variant="subtitle2" gutterBottom>
         Attach a Project Banner
@@ -109,44 +93,13 @@ export default function ProjectInfoForm() {
             </Button>
           </label>
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="state"
-            name="state"
-            label="State/Province/Region"
-            fullWidth
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="zip"
-            name="zip"
-            label="Zip / Postal code"
-            fullWidth
-            autoComplete="shipping postal-code"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="country"
-            name="country"
-            label="Country"
-            fullWidth
-            autoComplete="shipping country"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
-            label="Use this address for payment details"
-          />
-        </Grid>
+
+        <ProjectLinksForm/>
+        <Button variant="outlined" color="secondary" type="submit">Login</Button>
+        </Box>
       </Grid>
+        
+      
     </React.Fragment>
   );
 }
