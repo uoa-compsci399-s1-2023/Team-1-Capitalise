f508952ab608e075db55af8a8be3ca8b0b219c2a
NoGraphite <99214003+Dkqu@users.noreply.github.com>
2023-05-08 13:59:50 +1200

    Division of the Upload page into mini components

---
+++ src/components/upload/ProjectTeamSelection.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import { Autocomplete, Box, Button, Chip, FormControl, FormHelperText, Input, InputLabel, MenuItem, Select, SelectChangeEvent, styled } from '@mui/material';
+import { useState } from 'react';
+import ProjectLinksForm from './ProjectLinks';
+
+export default function ProjectTeamSelectionForm() {
+
+
+ 
+  return (
+    <React.Fragment>
+      <Typography variant="h6" gutterBottom>
+        1. Team Details
+      </Typography>
+      
+      
+    </React.Fragment>
+  );
+}
