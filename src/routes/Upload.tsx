9ecca9b4af6211c47f0d00969ef0af994996cc59
Daniel Quach <dqua365@aucklanduni.ac.nz>
2023-05-09 18:25:20 +1200

    Added comments and handleUpload()

--- src/routes/Upload.tsx
+++ src/routes/Upload.tsx
@@ -25,9 +25,18 @@ const steps = ['Team Details', 'Project Details', 'Project Files', 'Upload'];
 
 
 export default function Upload() {
+  //Holds the counter for navigation between pages.
   const [activeStep, setActiveStep] = useState(0);
+  //String of Team Name
   const[team, setTeam] = useState('');
+  //Array of Project attributes - Project Name, Project Semester, Project Category, Project Description
   const[projectInfo, setProjectInfo] = useState('');
+  //Banner Image
+  const[banner, setBanner] = useState('');
+  //Thumbnail
+  const[thumbnail, setThumbnail] = useState('');
+  //Array of Project Images
+  const[projectImages, setProjectImages] = useState('');
   const handleNext = () => {
     setActiveStep(activeStep + 1);
   };
@@ -52,22 +61,34 @@ export default function Upload() {
   }
 
   const projectFileToUpload = (banner: any, images: any, thumbnail: any) => {
-    //Stores Project Info into Project State
-    console.log(banner?.name);
-    console.log(images);
-    console.log(thumbnail?.name);
-    //Navigates to next page
+    //Stores Project Files into respective states
+    setBanner(banner);  
+    setProjectImages(images);  
+    setThumbnail(thumbnail);
+    //Navigates to loading page  
     handleNext();
-    
+    //Calls upload function as we have all the information to create a project
+    handleUpload();
   }
+
+  //The final call to create a Project
+  const handleUpload = () => {
+    //API Call here!
+
+  }
+  //This acts as a Navigator for each of upload pages rendered.
   function getStepContent(step: number) {
     switch (step) {
+      //Enter Team details - Team Name
       case 0: 
         return <ProjectTeamSelectionForm teamToUpload={teamToUpload}/>;
+      //Project Information - project name, semester, category, tags, description
       case 1:
         return <ProjectInfoForm  projectInfoToUpload={projectInfoToUpload} handleBack={handleBack}/>;
+      //Project File  Upload - Banner, thumbnail, and any related imags.
       case 2: 
-        return <ProjectUploadFileForm projectFileToUpload={projectFileToUpload} />; 
+        return <ProjectUploadFileForm projectFileToUpload={projectFileToUpload} handleBack={handleBack}/>; 
+      //An API call screen. Shows a successful response for Users.
       case 3:
         return <UploadComplete />;
       default:
