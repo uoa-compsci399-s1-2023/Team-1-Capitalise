b6c32fd90a03a6d742000b010d160ae584000f27
rwu050 <rwu050@aucklanduni.ac.nz>
2023-05-13 10:42:43 +1200

    changed interface

--- src/routes/AdminDashboard.tsx
+++ src/routes/AdminDashboard.tsx
@@ -1,4 +1,12 @@
-import { Box, Divider, Stack, Typography, useTheme } from "@mui/material";
+import {
+  Box,
+  Divider,
+  Stack,
+  Typography,
+  useTheme,
+  Paper,
+  colors,
+} from "@mui/material";
 import { useParams } from "react-router-dom";
 import { getUser } from "../api/getUser";
 import { TUser } from "../model/TUser";
@@ -10,46 +18,74 @@ import { getProject } from "../api/getProject";
 import ProjectsGrid from "../components/projects/ProjectsGrid";
 import MyButton from "../components/MyButton";
 import { error } from "console";
+import { Card, Container } from "@mui/material";
+import { ClearAll, DateRange, EmojiEvents } from "@mui/icons-material";
 
 const AdminDashboard = () => {
-    // need to connect up all the admin functionality we want here
-    // useEffect() can be used to load in all the stuff we might want to display.
+  // need to connect up all the admin functionality we want here
+  // useEffect() can be used to load in all the stuff we might want to display.
 
-    return (
-        <Box display={{ xs: "flex", md: "block" }} width="100%" >
-            <Box paddingTop={5} paddingLeft={25} width={{ xs: "25%", md: "80%" }} sx={{ aspectRatio: "1 / 1", objectFit: "cover", backgroundColor: 'primary.dark' }}>
-                <Typography variant="h1">ADMIN DASHBOARD</Typography>
-                <Divider />
-                <Stack paddingTop={2}>
-                    <Typography variant="h6">Welcome Admin</Typography>
-                </Stack>
-                <Stack paddingTop={1}>
-                    <Typography variant="h6">Here you will be able to carry out admin functions</Typography>
-                </Stack>
-                <Stack paddingTop={5}>
-                    <Box sx={{
-                    width: 1000,
-                    height: 500,
-                    backgroundColor: 'white',
-                    }}>
-                    </Box>
-                    
-                </Stack>
-                <Box sx={{
-                    width: 200,
-                    height: 200,
-                    backgroundColor: 'red',
-                    }}>
-                    </Box>
-                    <Box sx={{
-                    width: 200,
-                    height: 200,
-                    backgroundColor: 'green',
-                    }}>
-                    </Box>
-            </Box>
-        </Box>
-    );
+  return (
+    <Box flex={1} justifyContent={"center"}>
+      <Typography variant="h1" paddingTop={10} paddingLeft={10}>
+        Admin dashboard
+      </Typography>
+      <Divider />
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(auto-fill, 400px)"
+        flexDirection="row"
+        gap="50px"
+        justifyContent={"center"}
+        paddingTop={5}
+        paddingLeft={21}
+      >
+        <Paper elevation={3} sx={{ p: 3 }}>
+          <Typography variant="h4">Categories</Typography>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <ClearAll sx={{ height: 100, width: 100, opacity: 0.3, mr: 1 }} />
+            <Typography variant="h4">10</Typography>
+          </Box>
+        </Paper>
+
+        <Paper elevation={3} sx={{ p: 3 }}>
+          <Typography variant="h4">Semesters</Typography>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <DateRange sx={{ height: 100, width: 100, opacity: 0.3, mr: 1 }} />
+            <Typography variant="h4">10</Typography>
+          </Box>
+        </Paper>
+
+        <Paper elevation={3} sx={{ p: 3 }}>
+          <Typography variant="h4">Awards</Typography>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <EmojiEvents
+              sx={{ height: 100, width: 100, opacity: 0.3, mr: 1 }}
+            />
+            <Typography variant="h4">10</Typography>
+          </Box>
+        </Paper>
+      </Box>
+    </Box>
+  );
 };
 
 export default AdminDashboard;
