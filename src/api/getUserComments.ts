811461b6e1838f3994c8cdff755bfd1d779e5f6a
alt158 <126435639+AlbertLinNZ@users.noreply.github.com>
2023-05-12 13:15:38 +1200

    change back api

--- src/api/getUserComments.ts
+++ src/api/getUserComments.ts
@@ -1,9 +1,8 @@
 import { TComment } from "../model/TComment";
-const TEMP_API =
-  "https://mbw0ut8bmf.execute-api.ap-southeast-2.amazonaws.com/staging";
+import { API_URL } from "./config";
 
 export async function getUserComments(userId: string): Promise<TComment[]> {
-  const response = await fetch(`${TEMP_API}/api/users/myComments/${userId}`);
+  const response = await fetch(`${API_URL}/api/users/myComments/${userId}`);
   if (!response.ok) {
     response.text().then((err) => console.log(err));
     return [];
