0104fc674ffba52f516c5b5367fc16a703528ea3
NoGraphite <99214003+Dkqu@users.noreply.github.com>
2023-05-15 14:19:36 +1200

    Validation Project Info Implemented

--- src/components/upload/ProjectInfo.tsx
+++ src/components/upload/ProjectInfo.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
+import validator from "validator";
+import nw from "naughty-words";
 import {
   Autocomplete,
   Box,
@@ -35,59 +37,137 @@ interface TProjectInfo {
 export default function ProjectInfoForm(
   {projectInfoToUpload, handleBack }: any,
 ) {
-
-  const [selectedFile, setSelectedFile] = useState<File | null>(null);
-  const [selectedTags, setSelectedTags] = useState([]);
-  const [semester, setSemester] = React.useState("");
-  const [category, setCategory] = React.useState("");
-  const tagList : {name: string}[] = [];
   
+  //Project Name State
+  const [projectNameErrorText, setProjectNameErrorText] = useState('');
+  
+  //Project Description State 
+  const [projectDescErrorText, setProjectDescErrorText] = useState('');
+  
+  
+  //Semester States
+  const [semester, setSemester] = React.useState('');
+  const [semesterErrorText, setSemesterErrorText] = useState('');
+  
+  //Category States
+  const [category, setCategory] = React.useState('');
+  const [categoryErrorText, setCategoryErrorText] = useState('');
+  
+  //Tag states
+  const tagList : {name: string}[] = [];
+  const [selectedTags, setSelectedTags] = useState([]);
+  const [tagErrorText, setTagErrorText] = useState('');
+  
+  
+  const validateProjectName= (projectN: any) => { 
+    //Project Name Validation 
+    if(!validator.isAscii(projectN)) {
+      setProjectNameErrorText('Enter a project name with only ASCII characters.');
+      
+    } else if (!validator.isEmpty(projectN)) {
+      setProjectNameErrorText('Enter a project name.');
+    } else { 
+      setProjectNameErrorText('');
+      return true;
+    }
+  }
+  const validateProjectSemester= (sem: any) => { 
+    //Project Semester Validation 
+    if(!validator.isEmpty(sem)) {
+      setSemesterErrorText('Pick your project semester.');
+    } else { 
+      setSemesterErrorText('');
+      return true;
+    }
+  }
+  const validateProjectCategory= (cat: any) => { 
+    //Project Category Validation 
+    if(!validator.isEmpty(cat)) {
+      setCategoryErrorText('Pick a project category.');
+    } else { 
+      setCategoryErrorText('');
+      return true;
+    }
+  }
+  
+  const validateProjectTags= (selectedTags: any) => { 
+    //Project Tags Validation 
+    var msgError = 'The following tags are invalid: ';
+    if(selectedTags) {
+      for (var i = 0 ; i < selectedTags.length; i++) {
+        //Check against a blacklist, inappropriate word list.
+        if (nw.en.includes(selectedTags[i])) {
+          msgError += selectedTags[i] + ' ';   
+        }
+        
+      }
+      setTagErrorText(msgError);
+    } else { 
+      setTagErrorText('');
+      return true;
+    }
+  }
+  const validateProjectDesc= (projectDesc: any) => { 
+    //Project Description Validation 
+    if(!validator.isAscii(projectDesc)) {
+      setProjectDescErrorText('Enter a project description with only ASCII characters.');
+      
+    } else if (!validator.isEmpty(projectDesc)) {
+      setProjectNameErrorText('Enter a project description.');
+    } else { 
+      setProjectNameErrorText('');
+      return true;
+    }
+  }
+
+  //API Fetches for Semester and Category lists 
   // Map the semesters to the MenuItems.
-  const [semesters, setSemesters] = useState<JSX.Element[]>([]);
-  useEffect(() => {
-    // grab the semesters from the API
-    const fetchSemesters = getSemesters().then((data) => {
-      // Map the semesters to the MenuItem components
-      const semesters = data.map(semester => (
-        <MenuItem key={semester._id} value={semester.value}>
-          {semester.value}
-        </MenuItem>
-      ));
-      setSemesters(semesters);
-    })
-  }, []);
+    const [semesterList, setSemesterList] = useState<JSX.Element[]>([]);
+    useEffect(() => {
+      // grab the semesters from the API
+      const fetchSemesters = getSemesters().then((data) => {
+        // Map the semesters to the MenuItem components
+        const semesterList = data.map(semester => (
+          <MenuItem key={semester._id} value={semester.value}>
+            {semester.value}
+          </MenuItem>
+        ));
+        setSemesterList(semesterList);
+      })
+    }, []);
 
-  // Map the categories to the MenuItems.
-  const [categories, setCategories] = useState<JSX.Element[]>([]);
-  useEffect(() => {
-    // grab the categories from the API
-    const fetchCategories = getCategories().then((data) => {
-      // Map the categories to the MenuItem components
-      const categories = data.map(category => (
-        <MenuItem key={category._id} value={category.value}>
-          {category.value}
-        </MenuItem>
-      ));
-      setCategories(categories);
-    })
-  }, []);
+    // Map the categories to the MenuItems.
+    const [categories, setCategories] = useState<JSX.Element[]>([]);
+    useEffect(() => {
+      // grab the categories from the API
+      const fetchCategories = getCategories().then((data) => {
+        // Map the categories to the MenuItem components
+        const categories = data.map(category => (
+          <MenuItem key={category._id} value={category.value}>
+            {category.value}
+          </MenuItem>
+        ));
+        setCategories(categories);
+      })
+    }, []);
 
 
-
+  //Setting Category State on Change
   const handleCategoryChange = (event: SelectChangeEvent) => {
     event.preventDefault();
     setCategory(event.target.value);
   };
+  //Adding selected tags into a list state.
   const handleTagChange = (event: any, value: any) => {
     event.preventDefault();
     setSelectedTags(value);
   };
-
+  //Setting Semester state upon Change
   const handleSemesterChange = (event: SelectChangeEvent) => {
     event.preventDefault();
     setSemester(event.target.value);
   };
-
+  //Submitting data on Next press to the main Upload parent component to store.
   const handleNext = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -95,16 +175,32 @@ export default function ProjectInfoForm(
     const sem = semester;
     const cat = category;
     const projDesc = data.get("projectDesc") as string;
-    const infoSend: TProjectInfo = {
-      projN: projectN,
-      categoryN: cat,
-      semesterN: sem,
-      tags: selectedTags,
-      projectDescription: projDesc,
-    };
-    console.log("Info sent from ProjectInfo:", infoSend);
+    //Validation Checks on correct data.
+    const okProjectName = validateProjectName(projectN);
+    const okProjectSem = validateProjectSemester(sem);
+    const okProjectCat = validateProjectCategory(cat);
+    const okProjectDesc = validateProjectDesc(projDesc);
+    const okProjectTags = validateProjectTags(selectedTags);
+    
+    if (okProjectName && okProjectSem && okProjectCat && okProjectDesc && okProjectTags) {
+      const infoSend: TProjectInfo = {
+        projN: projectN,
+        categoryN: cat,
+        semesterN: sem,
+        tags: selectedTags,
+        projectDescription: projDesc,
+      };
+       //console.log("Info sent from ProjectInfo:", infoSend);
+      projectInfoToUpload(infoSend);
+      
+    }
+    
+    
+    
+    
+   
 
-    projectInfoToUpload(infoSend);
+   
   };
 
   return (
@@ -123,13 +219,13 @@ export default function ProjectInfoForm(
                 id="select-semester"
                 value={semester}
                 label="Semester"
+               
                 onChange={handleSemesterChange}
               >
-                {semesters}
+                {semesterList}
               </Select>
               <FormHelperText>
-                {" "}
-                * When did you complete this project?
+              {semesterErrorText ? semesterErrorText: '* When was this project completed?'}
               </FormHelperText>
             </FormControl>
           </Grid>
@@ -144,13 +240,15 @@ export default function ProjectInfoForm(
                 id="select-category"
                 value={category}
                 label="Cateogry"
+              
+          
                 onChange={handleCategoryChange}
               >
                 {categories}
               </Select>
               <FormHelperText>
-                {" "}
-                * What does this project specialise in?
+                {categoryErrorText ? categoryErrorText: '* What does this project specialise in?'}
+                
               </FormHelperText>
             </FormControl>
           </Grid>
@@ -163,6 +261,8 @@ export default function ProjectInfoForm(
               name="projectName"
               label="Project Name"
               fullWidth
+              error={!!projectNameErrorText}
+              helperText={projectNameErrorText ? projectNameErrorText : ''}
               variant="outlined"
             />
           </Grid>
@@ -190,6 +290,8 @@ export default function ProjectInfoForm(
                   {...params}
                   variant="outlined"
                   label="Project Tags"
+                  error={!!tagErrorText}
+                  helperText={tagErrorText? tagErrorText : ''}
                 />
               )}
             />
@@ -204,6 +306,8 @@ export default function ProjectInfoForm(
               variant="outlined"
               multiline={true}
               rows={10}
+              error={!!projectDescErrorText}
+              helperText={projectDescErrorText? projectDescErrorText : 'Enter a short description about your project'}
             />
           </Grid>
         </Grid>
