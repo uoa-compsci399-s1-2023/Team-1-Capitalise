7276f93d7e9c6dea593bbcbe82c9283c6250a725
rwu050 <rwu050@aucklanduni.ac.nz>
2023-05-15 15:27:42 +1200

    prevented non-admin users from accessing adminDashboard route

--- src/routes/AdminDashboard.tsx
+++ src/routes/AdminDashboard.tsx
@@ -1,30 +1,17 @@
-import {
-  Box,
-  Divider,
-  Stack,
-  Typography,
-  useTheme,
-  Paper,
-  colors,
-} from "@mui/material";
-import { useParams } from "react-router-dom";
-import { getUser } from "../api/getUser";
-import { TUser } from "../model/TUser";
-import { TProject } from "../model/TProject";
-import { useEffect, useState } from "react";
-import MyTabs from "../components/MyTabs";
-import ProjectCard from "../components/projects/ProjectCard";
-import { getProject } from "../api/getProject";
-import ProjectsGrid from "../components/projects/ProjectsGrid";
-import MyButton from "../components/MyButton";
-import { error } from "console";
-import { Card, Container } from "@mui/material";
-import { ClearAll, DateRange, EmojiEvents } from "@mui/icons-material";
 import Dashboard from "../components/adminDashboard/Dashboard";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../customHooks/useAuth";
+import React, { useEffect } from "react";
 
 const AdminDashboard = () => {
-  // need to connect up all the admin functionality we want here
-  // useEffect() can be used to load in all the stuff we might want to display.
+  const auth = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.user) {
+      navigate("/");
+    }
+  });
 
   return <Dashboard />;
 };
