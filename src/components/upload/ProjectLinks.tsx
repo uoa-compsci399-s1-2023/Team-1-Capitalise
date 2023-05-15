afbb07298ab4662be8344e20a3306a7804e33871
Daniel Quach <dqua365@aucklanduni.ac.nz>
2023-05-16 01:02:10 +1200

    starting validation for links

--- src/components/upload/ProjectLinks.tsx
+++ src/components/upload/ProjectLinks.tsx
@@ -3,6 +3,7 @@ import { CollectionsBookmarkOutlined } from "@mui/icons-material";
 import { FormControl, TextField, Grid, InputLabel, Select, MenuItem, Button, Typography, IconButton} from "@mui/material";
 import React, { useState } from "react";
 import DeleteIcon from '@mui/icons-material/Delete';
+import validator from 'validator';
 
 const LinkForm = styled(FormControl)({
     marginBottom: 8,
@@ -46,12 +47,15 @@ const options = [
   },
 ];
 export default function ProjectLinksForm({handleProjectLinks}: any) {
+
   const [selectedOptions, setSelectedOptions] = useState([{ value: '', type: '' }]);
+  console.log(selectedOptions[0].type);
   const handleOptionChange = (index:any, event:any) => {
     const newSelectedOptions = [...selectedOptions];
     newSelectedOptions[index].type = event.target.value;
     setSelectedOptions(newSelectedOptions);
   };
+
   const handleTextChange = (index:any, event:any) => {
     const newSelectedOptions = [...selectedOptions];
     newSelectedOptions[index].value = event.target.value;
@@ -69,6 +73,20 @@ export default function ProjectLinksForm({handleProjectLinks}: any) {
     newSelectedOptions.splice(index, 1);
     setSelectedOptions(newSelectedOptions);
   };
+    //Validate the Links are appropriate
+    const validateLinks = () => {
+      for (const link in selectedOptions) {
+        if(link == 'github') {
+  
+        }
+  
+      }
+     
+  
+  
+    }
+
+
 
   return (
     <React.Fragment>
@@ -78,40 +96,37 @@ export default function ProjectLinksForm({handleProjectLinks}: any) {
         Project Resources
       </Typography>
     {selectedOptions.map((selectedOption, index) => (
-    <Grid item xs={12} key={index}>
-      <LinkForm>
-        <Select
-          labelId={`select-label-${index}`}
-          value={selectedOption.type}
-          onChange={(event) => handleOptionChange(index, event)}
-          autoWidth
-        >
-          {options.map((option) => (
-            <MenuItem key={option.type} value={option.type}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </Select>
+      <Grid item xs={12} key={index}>
+        <LinkForm>
+          <Select
+            labelId={`select-label-${index}`}
+            value={selectedOption.type}
+            onChange={(event) => handleOptionChange(index, event)}
+            autoWidth>
+            {options.map((option) => (
+              <MenuItem key={option.type} value={option.type}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </Select>
       </LinkForm>
-
+      {/*Text Field for Links*/}
       {selectedOption.type !== '' &&  (
         <SocialLinkField
           fullWidth
           label="Insert a Link"
           value={selectedOption.value}
           onChange={(event) => handleTextChange(index, event)}
-        />
-      )}
+        />)}
+      {/*Delete Link Field*/}
       {selectedOptions.length > 1 && (
-    <IconButton onClick={() => handleRemove(index)}>
-      <DeleteIcon />
-    </IconButton>
+        <IconButton onClick={() => handleRemove(index)}>
+          <DeleteIcon />
+        </IconButton>
       )}
     </Grid>
-    
-    
   ))}
-
+  {/*Add Extra Link Button sits outside Grid*/}
   {selectedOptions.length < 5 && ( <Button onClick={handleAdd}>Add more links  </Button>)}
     </Grid>
 
