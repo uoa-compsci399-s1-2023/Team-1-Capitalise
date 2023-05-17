b4a436e373adcb3706089d9b1a68edc0323a509c
AlbertLinNZ <ablin158@gmail.com>
2023-05-17 12:44:53 +1200

    editing data now working

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,30 +1,103 @@
-import { Box, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  FormControlLabel,
+  Stack,
+  Switch,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
 
 const About = () => {
-  const mockAboutData = [
+  const [mockAboutData, setMockAboutData] = useState([
     { title: "What is Capitalise?", body: "this is the body" },
     { title: "So, what is Capstone?", body: "this is the body" },
-  ];
+  ]);
 
-  mockAboutData.map((block) => {
-    console.log(block.title);
-    console.log(block.body);
-  });
+  const [editChecked, setEditChecked] = useState(false);
+
+  const handleEditChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEditChecked(event.target.checked);
+  };
+
+  const handleEditSubmit = (title: string, body: string, index: number) => {
+    let newData = mockAboutData;
+    newData[index] = { title: title, body: body };
+    setMockAboutData(newData);
+    console.log("set");
+    console.log(mockAboutData);
+  };
 
   return (
     <Stack
+      position="relative"
       padding="50px 0px"
       gap="100px"
-      width="1000px"
+      width="65%"
       margin="auto"
       minHeight="92vh"
     >
-      {mockAboutData.map((section) => (
-        <Stack gap="10px">
-          <Typography variant="h1">{section.title}</Typography>
-          <Typography variant="body1">{section.body}</Typography>
-        </Stack>
-      ))}
+      <Box position="absolute" top="15px" right="0px">
+        <FormControlLabel
+          control={
+            <Switch
+              checked={editChecked}
+              onChange={handleEditChange}
+              inputProps={{ "aria-label": "controlled" }}
+            />
+          }
+          label="Edit"
+        />
+      </Box>
+
+      {!editChecked &&
+        mockAboutData.map((section, i) => (
+          <Stack gap="10px" key={i}>
+            <Typography variant="h1">{section.title}</Typography>
+            <Typography variant="body1">{section.body}</Typography>
+          </Stack>
+        ))}
+
+      {editChecked &&
+        mockAboutData.map((section, i) => {
+          let title = section.title;
+          let body = section.body;
+          return (
+            <Stack direction="row" key={i} gap="10px" alignItems="center">
+              <Stack gap="10px" width="100%">
+                <TextField
+                  margin="dense"
+                  label="Title"
+                  multiline
+                  fullWidth
+                  variant="outlined"
+                  defaultValue={section.title}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    title = event.target.value;
+                  }}
+                />
+                <TextField
+                  margin="dense"
+                  label="Body"
+                  multiline
+                  fullWidth
+                  variant="outlined"
+                  defaultValue={section.body}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    body = event.target.value;
+                  }}
+                />
+              </Stack>
+              <Button
+                variant="contained"
+                onClick={() => handleEditSubmit(title, body, i)}
+              >
+                {"submit"}
+              </Button>
+            </Stack>
+          );
+        })}
     </Stack>
   );
 };
