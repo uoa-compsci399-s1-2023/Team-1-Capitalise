6e994614cbc6ec9b4ad08a55bdae73185c16ef03
AlbertLinNZ <ablin158@gmail.com>
2023-05-17 12:07:35 +1200

    basic logic for about page

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,9 +1,31 @@
+import { Box, Stack, Typography } from "@mui/material";
+
 const About = () => {
+  const mockAboutData = [
+    { title: "What is Capitalise?", body: "this is the body" },
+    { title: "So, what is Capstone?", body: "this is the body" },
+  ];
+
+  mockAboutData.map((block) => {
+    console.log(block.title);
+    console.log(block.body);
+  });
 
   return (
-    <div className="About">
-      <h1>About</h1>
-    </div>
+    <Stack
+      padding="50px 0px"
+      gap="100px"
+      width="1000px"
+      margin="auto"
+      minHeight="92vh"
+    >
+      {mockAboutData.map((section) => (
+        <Stack gap="10px">
+          <Typography variant="h1">{section.title}</Typography>
+          <Typography variant="body1">{section.body}</Typography>
+        </Stack>
+      ))}
+    </Stack>
   );
 };
 
