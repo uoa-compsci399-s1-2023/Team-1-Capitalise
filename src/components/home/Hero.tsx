3b39e8006826d36616ee90f468d9aff5f8f5c918
AlbertLinNZ <ablin158@gmail.com>
2023-05-18 18:11:57 +1200

    changed if logged into admin back to default message

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -59,7 +59,7 @@ const Hero = () => {
   useEffect(() => {
     const checkAuth = () => {
       if (auth.user) {
-        if (auth.user.userType == "graduate" || auth.user.userType == "admin") {
+        if (auth.user.userType == "graduate") {
           setIsStudent(true);
           if (auth.user.project) {
             setHasProject(true);
