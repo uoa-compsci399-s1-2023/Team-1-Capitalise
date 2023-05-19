73f13e703a1dc75ec49c470bd12a4efd25b0b897
DQ <99214003+Dkqu@users.noreply.github.com>
2023-05-19 22:40:59 +1200

    Update postBanner.ts

--- src/api/postBanner.ts
+++ src/api/postBanner.ts
@@ -1,7 +1,7 @@
 import { S3_API_URL } from "./config";
 
 export async function postBanner(projectId: string, banner: FormData) {
-  const response = await fetch(`${S3_API_URL}/banner/${projectId}`, {
+  const response = await fetch(`${S3_API_URL}/api/s3/banner/${projectId}`, {
     method: "POST",
     body: banner,
   });
