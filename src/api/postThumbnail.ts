84877799ef206e538fd665125efd83bc95d4181b
DQ <99214003+Dkqu@users.noreply.github.com>
2023-05-19 22:42:15 +1200

    Update postThumbnail.ts

--- src/api/postThumbnail.ts
+++ src/api/postThumbnail.ts
@@ -1,7 +1,7 @@
 import { S3_API_URL } from "./config";
 
 export async function postThumbnail(projectId: string, thumbnail: FormData) {
-  const response = await fetch(`${S3_API_URL}/thumbnail/${projectId}`, {
+  const response = await fetch(`${S3_API_URL}/api/s3/thumbnail/${projectId}`, {
     method: "POST",
     body: thumbnail,
   });
