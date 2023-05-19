01deb8a82e1e35ed9fa3529e63769ce383d6ccc9
DQ <99214003+Dkqu@users.noreply.github.com>
2023-05-19 22:45:07 +1200

    Update config.ts

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,7 @@
 export const API_URL =
-  "https://bh71phacjb.execute-api.ap-southeast-2.amazonaws.com";
- //"https://mbw0ut8bmf.execute-api.ap-southeast-2.amazonaws.com/staging";
+  "http://3.27.68.144:3000";
+  // "https://bh71phacjb.execute-api.ap-southeast-2.amazonaws.com";
+  // "https://mbw0ut8bmf.execute-api.ap-southeast-2.amazonaws.com/staging";
 
 export const S3_API_URL =
   "https://l27nrectig.execute-api.ap-southeast-2.amazonaws.com/dev;
