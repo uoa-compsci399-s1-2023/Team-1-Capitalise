cd092bef9ff3df5c902582331eb1ff338b2d9236
yathk <ykid285@aucklanduni.ac.nz>
2023-05-20 20:54:33 +1200

    fixed config

--- src/api/config.ts
+++ src/api/config.ts
@@ -5,5 +5,5 @@ export const API_URL =
   // "https://bh71phacjb.execute-api.ap-southeast-2.amazonaws.com";
   // "https://mbw0ut8bmf.execute-api.ap-southeast-2.amazonaws.com/staging";
 export const S3_API_URL = 
-    // API_URL;
+    API_URL;
 // "https://mbw0ut8bmf.execute-api.ap-southeast-2.amazonaws.com/staging";
