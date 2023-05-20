ca8c8688f0c7ac72acb00129a952293acb468959
lucaseng <lucasengnz@outlook.com>
2023-05-21 00:41:10 +1200

    Maybe a CORS fix

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const app = express();
 
 // Enable CORS for all methods
 app.use(cors())
+/*
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*")
 	res.header("Access-Control-Allow-Headers", "*, x-auth-token")
@@ -33,7 +34,7 @@ app.use(function(req, res, next) {
 	  }
 	
 	next()
-  });
+  });*/
 
 app.use(
   session({
