59062353a74697cf076cc43cacb7d734a0550e85
lucaseng <lucasengnz@outlook.com>
2023-05-21 00:12:54 +1200

    Added cors

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const { Project } = require("./models/project");
 const app = express();
 
 // Enable CORS for all methods
+app.use(cors())
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*")
 	res.header("Access-Control-Allow-Headers", "*, x-auth-token")
