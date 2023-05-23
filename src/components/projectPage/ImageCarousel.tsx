bdc954523c4c7788b9b058b65d5ff558637682c5
yathk <ykid285@aucklanduni.ac.nz>
2023-05-24 02:34:17 +1200

    Added responsive sizing for videos and images

--- src/components/projectPage/ImageCarousel.tsx
+++ src/components/projectPage/ImageCarousel.tsx
@@ -1,49 +1,58 @@
-import React, { useEffect, memo } from 'react'
+import React, { useEffect, memo, useState, useRef } from 'react'
 import Carousel from 'react-material-ui-carousel'
-import { Stack } from '@mui/material'
+import { Stack, useMediaQuery } from '@mui/material'
 
-export function Image({ url }: { url: string }) {
+export function Image({ url, parentWidth }: { url: string, parentWidth: number }) {
+
+  // 16:9 ratio
+  const height = parentWidth * 0.5625;
+
   return (
     <Stack
       width='100%'
+      height={height}
     >
       <a
         href={url}
         target='_blank'
         style={{
-          margin: '0 auto'
+          width: '100%',
+          height: '100%'
         }}
       >
         <img
           src={url}
           width={'100%'}
-          height={'400px'}
+          height={'100%'}
           style={{
-            objectFit: 'scale-down',
+            objectFit: 'contain',
           }}
           alt="Gallery Image"
         />
+
       </a>
     </Stack>
   )
 }
 
-export function ImageCarousel({ urls }: { urls: string[] }) {
+export function ImageCarousel({ urls, parentWidth }: { urls: string[], parentWidth: number }) {
+  const height = parentWidth * 0.5625;
 
   return (
-    <Carousel
-      animation='slide'
-      duration={600}
-      sx={{ mt: 1 }}
-      autoPlay={false}
-      cycleNavigation={false}
+      <Carousel
+        height={height}
+        animation='slide'
+        duration={600}
+        // sx={{ mt: 1 }}
+        autoPlay={false}
+        cycleNavigation={false}
       // navButtonsAlwaysVisible
-    >
-      {urls.map((imgUrl, index) => (
-        <Image url={imgUrl} key={index} />
-      ))}
-    </Carousel>
-  )
+      >
+        {urls.map((imgUrl, index) => (
+          <Image parentWidth={parentWidth} url={imgUrl} key={index} />
+        ))}
+      </Carousel>
+      )
 }
 
-export const MemoizedImageCarousel = memo(ImageCarousel)
+      export const MemoizedImageCarousel = memo(ImageCarousel)
